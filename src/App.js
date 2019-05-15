f367278e5e9514898f8cd43160dfb487ba9b2100
Jesus Cebader <zebader@hotmail.com>
2019-05-15 12:53:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import data from './data/foods.json';
+import 'bulma/css/bulma.css';
 import './App.css';
 
+import FoodBox from './components/FoodBox'
+
 class App extends Component {
+
+  state = {
+    data: data
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FoodBox />
       </div>
     );
   }
